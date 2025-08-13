b505d06c4fa63ae122f41afe44dd43222af1a5e0
Salvatore Criscione <not.salvatore@gmail.com>
2025-08-13 21:31:45 +0300

    update

--- modules/system/services.nix
+++ modules/system/services.nix
@@ -26,6 +26,25 @@
 
     udev.enable = true;
 
+    postgresql = {
+      enable = true;
+      ensureDatabases = [ "postgres" ];
+      enableTCPIP = true;
+      settings = {
+        max_connections = 5000;
+      };
+      # port = 5432;
+      authentication = pkgs.lib.mkOverride 10 ''
+        #...
+        #type database DBuser origin-address auth-method
+        local all       all     trust
+        # ipv4
+        host  all      all     127.0.0.1/32   trust
+        # ipv6
+        host all       all     ::1/128        trust
+      '';
+    };
+
     fstrim.enable = lib.mkDefault true;
 
     # --- [ Touchpad ] ---
